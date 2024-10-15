85b112585ba4699b7ba68af095451669c4bf9f61
Yon Longdy <yon.longdy.1221@rupp.edu.kh>
2024-10-15 14:38:37 +0700

    finish

---
+++ Fixedbug.java
@@ -0,0 +1,6 @@
+public class Fixedbug {
+    public static void main(String[] args) {
+        System.out.println("UI.java");
+        System.out.println("Verson 1");
+    }
+}
--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Main.java");
         System.out.println("Verson 2");
         System.out.println("added code for verson2");
+        System.out.println("Fixedbug me bug ");
     }
 }
